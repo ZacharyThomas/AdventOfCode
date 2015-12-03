691a48572dfa015fcdbf856f85b23e9ba7a93a47
Zachary.smith <Zachary.smith@yodle.com>
2015-12-03 00:17:29 -0500

    I made it work real good for day 2

---
+++ day_2/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to day_2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ day_2/project.clj
@@ -0,0 +1,9 @@
+(defproject day_2 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot day-2.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
